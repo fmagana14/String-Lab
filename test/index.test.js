a788d73ed0ddff7eb85d38cca041bfc3dc74ec96
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-06-27 14:11:48 -0700

    cleaned up testing

--- test/index.test.js
+++ test/index.test.js
@@ -24,19 +24,19 @@ test("test lowercase to uppercase", () => {
     expect(allCaps("foo bar")).toBe("FOO BAR");
     expect(allCaps("hello world")).toBe("HELLO WORLD");
 });
-
+// kebob test
 test("testing kebobcase", () => {
     expect(kebobCase("the game")).toBe("the-game");
     expect(kebobCase("foo bar")).toBe("foo-bar");
     expect(kebobCase("hello world")).toBe("hello-world");
 });
-
+// removeExtraSpace Test
 test("testing removeExtraSpaces", () => {
     expect(removeExtraSpaces(" the game")).toBe("the game");
     expect(removeExtraSpaces("  foo  bar  ")).toBe("foo bar");
     expect(removeExtraSpaces("     hello  world ")).toBe("hello world");
 });
-
+// snake test
 test("testing snake", () => {
     expect(snake("the game was fun")).toBe("the_game_was_fun");
     expect(snake("foo bar sucked")).toBe("foo_bar_sucked");
